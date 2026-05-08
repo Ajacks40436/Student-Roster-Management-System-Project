514cde510ed8d29a2b8b54e01a2b3030cad19800
Aliyah Jackson <aliyahjackson@Mac.lan>
2026-05-07 20:13:14 -0400

    Initial project setup and defined DegreeProgram

---
+++ degree.h
@@ -0,0 +1,17 @@
+//
+//  degree.h
+//  C867 Project
+//
+//  Created by Aliyah Jackson on 5/7/26.
+
+#ifndef DEGREE_H
+#define DEGREE_H
+
+enum DegreeProgram {
+    SECURITY,
+    NETWORK,
+    SOFTWARE
+};
+
+#endif
+
